d05fae8d987520bd4c915b5151847758a30580b6
elisevgeniy <elisevgeniy@gmail.com>
2024-06-28 18:00:09 +0300

    - support loading additional chapters

--- src/ru/mangabuff/src/eu/kanade/tachiyomi/extension/ru/mangabuff/MangaBuff.kt
+++ src/ru/mangabuff/src/eu/kanade/tachiyomi/extension/ru/mangabuff/MangaBuff.kt
@@ -1,7 +1,9 @@
 package eu.kanade.tachiyomi.extension.ru.mangabuff
 
+import android.annotation.SuppressLint
 import android.util.Log
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.POST
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
@@ -11,11 +13,18 @@ import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Headers
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
+import org.json.JSONObject
+import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import org.jsoup.parser.Parser
+import org.jsoup.select.Elements
+import rx.Observable
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -39,7 +48,7 @@ class MangaBuff : ParsedHttpSource() {
     override val id: Long = 1
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        Log.d("MangaBuff", "searchMangaRequest")
+//        Log.d("MangaBuff", "searchMangaRequest")
         var pageNum = 1
         when {
             page < 1 -> pageNum = 1
@@ -61,12 +70,12 @@ class MangaBuff : ParsedHttpSource() {
         .build()
 
     override fun popularMangaRequest(page: Int): Request {
-        Log.d("MangaBuff", "popularMangaRequest, page=$page")
+//        Log.d("MangaBuff", "popularMangaRequest, page=$page")
         return GET("$baseUrl/manga?page=$page", headers)
     }
 
     override fun latestUpdatesRequest(page: Int): Request {
-        Log.d("MangaBuff", "latestUpdatesRequest")
+//        Log.d("MangaBuff", "latestUpdatesRequest")
         return GET("$baseUrl/manga?sort=updated_at}")
     }
 
@@ -77,7 +86,7 @@ class MangaBuff : ParsedHttpSource() {
     override fun searchMangaSelector() = popularMangaSelector()
 
     override fun popularMangaFromElement(element: Element): SManga {
-        Log.d("MangaBuff", "MangaFromElement") // \n" + element.toString())
+//        Log.d("MangaBuff", "MangaFromElement") // \n" + element.toString())
         val manga = SManga.create()
         manga.thumbnail_url = baseUrl + element.select("div.cards__img").first()!!
             .attr("style")
@@ -144,7 +153,7 @@ class MangaBuff : ParsedHttpSource() {
     }
 
     override fun mangaDetailsParse(document: Document): SManga {
-        Log.d("MangaBuff", "mangaDetailsParse") // , document:\n" + document.toString())
+//        Log.d("MangaBuff", "mangaDetailsParse") // , document:\n" + document.toString())
         val descElement = document.select("div.manga__description").first()!!
         // Log.d("MangaBuff", "descElement:\n" + descElement)
         val status = document.select("a.manga__middle-link").get(2)!!.text()
@@ -158,16 +167,16 @@ class MangaBuff : ParsedHttpSource() {
         manga.status = parseStatus(status)
         manga.description = descElement.text().trim()
         manga.thumbnail_url = baseUrl + document.select(".manga__img > img").first()!!.attr("src")
-        Log.d(
-            "MangaBuff",
-            "manga:\n" +
-                "title: " + manga.title + "\n" +
-                "author: " + manga.author + "\n" +
-                "genre: " + manga.genre + "\n" +
-                "status: " + manga.status + "\n" +
-                "description: " + manga.description + "\n" +
-                "thumbnail_url: " + manga.thumbnail_url + "\n",
-        )
+//        Log.d(
+//            "MangaBuff",
+//            "manga:\n" +
+//                "title: " + manga.title + "\n" +
+//                "author: " + manga.author + "\n" +
+//                "genre: " + manga.genre + "\n" +
+//                "status: " + manga.status + "\n" +
+//                "description: " + manga.description + "\n" +
+//                "thumbnail_url: " + manga.thumbnail_url + "\n",
+//        )
         return manga
     }
 
@@ -181,17 +190,75 @@ class MangaBuff : ParsedHttpSource() {
 
     override fun chapterListSelector() = ".chapters__item"
 
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun fetchChapterList(manga: SManga): Observable<List<SChapter>> {
+//        Log.d("MangaBuff", "fetchChapterList") // , element:\n" + element.toString())
+
+        val response = client.newCall(chapterListRequest(manga)).execute()
+
+        val document = response.asJsoup()
+//        val script = document.selectFirst("script:containsData(const CHAPTER_ID)")?.data()
+//            ?: throw java.lang.Exception("Failed to get chapter id")
+
+        val mangaId = document.select(".manga").attr("data-id")
+        val csrfToken = document.select("meta[name=csrf-token]").attr("content")
+
+        val requestHeaders = headersBuilder().apply {
+//            add("Accept", "*/*")
+//            add("Host", baseUrl.toHttpUrl().host)
+//            set("Referer", response.request.url.toString())
+//            add("X-Requested-With", "XMLHttpRequest")
+            add("X-CSRF-TOKEN", csrfToken)
+        }.build()
+
+        val requestBody = "manga_id=$mangaId".toRequestBody("application/x-www-form-urlencoded; charset=UTF-8".toMediaTypeOrNull())
+
+        val ajaxResponse = client.newCall(
+            POST("$baseUrl/chapters/load", requestHeaders, requestBody),
+        ).execute()
+
+        var data = ajaxResponse
+//        Log.d("MangaBuff", "ajaxResponse.asJsoup():\n" + ajaxResponse.asJsoup())
+        val jsonData: String = data.body.string()
+//        Log.d("MangaBuff", "jsonData:\n" + jsonData)
+        val Jobject = JSONObject(jsonData)
+//        Log.d("MangaBuff", "Jobject:\n" + Jobject.toString())
+//        Log.d("MangaBuff", "Jobject.has(\"content\"):\n" + Jobject.has("content"))
+        val content = Jobject.get("content").toString()
+//        Log.d("MangaBuff", "Jobject.get(content):\n" + content)
+        val additionalDocument = Jsoup.parse(content, "", Parser.xmlParser())
+
+        var newerChapters: Elements = document.select(".chapters__item")
+        var olderChapters: Elements = additionalDocument.select(".chapters__item")
+
+        val chapters = mutableListOf<SChapter>()
+
+        for (el: Element in newerChapters) {
+            chapters.add(chapterFromElement(el))
+        }
+        for (el: Element in olderChapters) {
+            chapters.add(chapterFromElement(el))
+        }
+
+        return Observable.just(chapters)
+
+//        Log.d("MangaBuff", "csrfToken:\n" + csrfToken) // , element:\n" + element.toString())
+//        Log.d("MangaBuff", "csrfToken:\n" + csrfToken) // , element:\n" + element.toString())
+//        Log.d("MangaBuff", "ajaxResponse:\n" + data) // , element:\n" + element.toString())
+//        Log.d("MangaBuff", "ajaxResponse body:\n" + data.toResponseBody("application/json; charset=utf-8".toMediaTypeOrNull()).toString()) // , element:\n" + element.toString())
+    }
+
     override fun chapterFromElement(element: Element): SChapter {
-        Log.d("MangaBuff", "chapterFromElement") // , element:\n" + element.toString())
-        Log.d("MangaBuff", "setUrlWithoutDomain:\n" + element.attr("href"))
-        Log.d(
-            "MangaBuff",
-            "name:\n" + element.select(".chapters__volume").first()!!.text() +
-                " " + element.select(".chapters__value").first()!!.text() +
-                " " + element.select(".chapters__name").first()!!.text(),
-        )
-        Log.d("MangaBuff", "chapter_number:\n" + element.select("div.chapters__value > span").first()!!.text().toFloat())
-        Log.d("MangaBuff", "date_upload:\n" + element.select("div.chapters__add-date").first()!!.text())
+//        Log.d("MangaBuff", "chapterFromElement") // , element:\n" + element.toString())
+//        Log.d("MangaBuff", "setUrlWithoutDomain:\n" + element.attr("href"))
+//        Log.d(
+//            "MangaBuff",
+//            "name:\n" + element.select(".chapters__volume").first()!!.text() +
+//                " " + element.select(".chapters__value").first()!!.text() +
+//                " " + element.select(".chapters__name").first()!!.text(),
+//        )
+//        Log.d("MangaBuff", "chapter_number:\n" + element.select("div.chapters__value > span").first()!!.text().toFloat())
+//        Log.d("MangaBuff", "date_upload:\n" + element.select("div.chapters__add-date").first()!!.text())
 
         val chapter = SChapter.create()
         chapter.setUrlWithoutDomain(element.attr("href"))
@@ -205,33 +272,45 @@ class MangaBuff : ParsedHttpSource() {
         } catch (e: ParseException) {
             chapter.date_upload = 0L
         }
-        Log.d(
-            "MangaBuff",
-            "chapter:\n" +
-                "name: " + chapter.name + "\n" +
-                "chapter_number: " + chapter.chapter_number + "\n" +
-                "date_upload: " + chapter.date_upload + "\n" +
-                "url: " + chapter.url + "\n",
-        )
+//        Log.d(
+//            "MangaBuff",
+//            "chapter:\n" +
+//                "name: " + chapter.name + "\n" +
+//                "chapter_number: " + chapter.chapter_number + "\n" +
+//                "date_upload: " + chapter.date_upload + "\n" +
+//                "url: " + chapter.url + "\n",
+//        )
         return chapter
     }
 
     override fun pageListParse(response: Response): List<Page> {
-        Log.d("MangaBuff", "pageListParse, response:\n" + response.toString())
+//        Log.d("MangaBuff", "pageListParse, response:\n" + response.toString())
         val body = response.asJsoup()
-        val urlElems = body.select(".reader__item")
+        val urlElems = body.select(".reader__item > img")
 
-        val pageUrls = arrayListOf<String>()
+        val pageUrls = ArrayList<String>()
         for (urlElem in urlElems) {
-            pageUrls.add(
-                urlElem.select("img").attr("src"),
-            )
+//            Log.d(
+//                "MangaBuff",
+//                "urlElem: " + urlElem.toString() +
+//                    "\n url: " + urlElem.attr("src"),
+//            )
+
+            if (urlElem.hasAttr("src")) {
+                pageUrls.add(
+                    urlElem.attr("src"),
+                )
+            } else {
+                pageUrls.add(
+                    urlElem.attr("data-src"),
+                )
+            }
         }
-        Log.d(
-            "MangaBuff",
-            "pageUrls:\n" +
-                "pageUrls: " + pageUrls.toString(),
-        )
+//        Log.d(
+//            "MangaBuff",
+//            "pageUrls (" + pageUrls.size + ":\n" +
+//                "pageUrls: " + pageUrls.toString(),
+//        )
 
         return pageUrls.mapIndexed { i, url -> Page(i, "", url) }
     }
